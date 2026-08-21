1f54834fed47bee85f8bdb3b934a53a82a8ccb03
koutyuke <75959529+koutyuke@users.noreply.github.com>
2026-08-21 23:43:37 +0900

    style: apply four-space Rust formatting

--- saba_core/src/error.rs
+++ saba_core/src/error.rs
@@ -2,8 +2,8 @@ use alloc::string::String;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Error {
-  Network(String),
-  UnexpectedInput(String),
-  InvalidUI(String),
-  Other(String),
+    Network(String),
+    UnexpectedInput(String),
+    InvalidUI(String),
+    Other(String),
 }
--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,16 @@ use net_wasabi::http::HttpClient;
 use noli::prelude::*;
 
 fn main() -> u64 {
-  let client = HttpClient::new();
-  match client.get("host.test".to_string(), 3000, "".to_string()) {
-    Ok(response) => {
-      print!("response:\n{:#?}", response);
+    let client = HttpClient::new();
+    match client.get("host.test".to_string(), 3000, "".to_string()) {
+        Ok(response) => {
+            print!("response:\n{:#?}", response);
+        }
+        Err(err) => {
+            print!("error:\n{:#?}", err);
+        }
     }
-    Err(err) => {
-      print!("error:\n{:#?}", err);
-    }
-  }
-  0
+    0
 }
 
 entry_point!(main);
